1a52e2be9c961daabd6c1d91b61e9af9fcaffbe4
irwincaliguiran-gh <irwincaliguiran@gmail.com>
2025-10-01 15:55:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbw1hL2ieVNC2dOmT2AwUgQgOgTPaNPFH1PfUZ1IDTkVmjygCUnxssirKt9F5Q3_j_JY/exec';
 
-function show(id) {
-  document.getElementById('loginForm').style.display = id==='loginForm' ? 'block' : 'none';
-  document.getElementById('registerForm').style.display = id==='registerForm' ? 'block' : 'none';
+function show(formId) {
+  document.getElementById('loginForm').style.display = formId === 'loginForm' ? 'block' : 'none';
+  document.getElementById('registerForm').style.display = formId === 'registerForm' ? 'block' : 'none';
 }
 
 function hashPwd(pwd) {
   return CryptoJS.SHA256(pwd).toString();
 }
 
-async function signIn(event) {
-  event.preventDefault();
+async function signIn(e) {
+  e.preventDefault();
   const email = document.getElementById('loginEmail').value;
   const password = hashPwd(document.getElementById('loginPass').value);
   const res = await fetch(API_URL, {
@@ -22,9 +22,9 @@ async function signIn(event) {
   else alert(message);
 }
 
-async function register(event) {
-  event.preventDefault();
-  const data = {
+async function register(e) {
+  e.preventDefault();
+  const userData = {
     action: 'register',
     username: document.getElementById('regUser').value,
     email: document.getElementById('regEmail').value,
@@ -34,7 +34,7 @@ async function register(event) {
   };
   const res = await fetch(API_URL, {
     method: 'POST',
-    body: JSON.stringify(data)
+    body: JSON.stringify(userData)
   });
   const { message } = await res.json();
   alert(message);
